46187d3e6378549511c16213aeed79c5cf1b05bd
roflanpotsan <l1sZ@ya.ru>
2022-04-20 23:10:56 +0300

    upd

--- cmake/projects/factorial/hunter.cmake
+++ cmake/projects/factorial/hunter.cmake
@@ -10,11 +10,11 @@ hunter_add_version(
     PACKAGE_NAME
     factorial
     VERSION
-    "1.0.0"
+    "1.0.1"
     URL
-    "https://github.com/roflanpotsan/factorial/archive/v1.0.0.tar.gz"
+    "https://github.com/roflanpotsan/factorial/archive/v1.0.1.tar.gz"
     SHA1
-    24414c3ef9f3ada042f71b56aead5867beef2f4e
+    e8bd6577645050206e40b4a1d333cea7ee511d5
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
